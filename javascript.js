7a2a6ed6162a68bab6252d03fac9ee1a267abf13
Kato <katuninecm@gmail.com>
2022-10-31 09:00:42 +0100

    PC can select one of the 3 options now

--- javascript.js
+++ javascript.js
@@ -1,5 +1,15 @@
 function getPcChoice (){
     let pcChoice = Math.floor(Math.random() * 3);
-    console.log(pcChoice);
-    return pcChoice;
+
+    if(pcChoice == 0){
+        console.log("Rock");
+       // return "Rock";
+    }else if(pcChoice == 1){
+        console.log("Paper");
+       // return "Paper";
+    }else{
+        console.log("Scissors");
+       // return "Scissors";
+    }
+    
 }
