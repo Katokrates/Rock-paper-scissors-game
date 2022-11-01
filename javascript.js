7100549f2df16efe6ea230a0165eb5ca999468ce
Kato <katuninecm@gmail.com>
2022-11-01 15:46:19 +0100

    Finished the function to play Rock paper scissors

--- javascript.js
+++ javascript.js
@@ -15,7 +15,44 @@ function getPcChoice (){
 function playRound (){
     let playerChoice = prompt("Please choose between Rock, Paper or Scissors.");
     let playerTransformedChoide = playerChoice.toLowerCase();
-    console.log(playerTransformedChoide);
     getPcChoice();
+
+    switch(playerTransformedChoide){
+        case "rock":
+            if(pcChosen == "Rock"){
+                console.log("Rock and Rock. It's a tie!")
+            }else if(pcChosen == "Paper"){
+                console.log("Rock and Paper. You lose!")
+            }else{
+                console.log("Rock and Scissors. You win!")
+            }
+        break;
+        
+        case "paper":
+            case "rock":
+                if(pcChosen == "Rock"){
+                    console.log("Paper and Rock. You win!")
+                }else if(pcChosen == "Paper"){
+                    console.log("Paper and Paper. It's a tie!")
+                }else{
+                    console.log("Paper and Scissors. You lose!")
+                }
+            break;
+        
+        case "scissors":
+            case "rock":
+            if(pcChosen == "Rock"){
+                console.log("Scissors and Rock. You lose")
+            }else if(pcChosen == "Paper"){
+                console.log("Scissors and Paper. You win!")
+            }else{
+                console.log("Scissors and Scissors. It's a tie!")
+            }
+        break;
+
+        default:
+            console.log("Something went wrong");
+
+    }
     
 }
